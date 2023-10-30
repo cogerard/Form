8eb7878f1601ead0451e077d9013d4eb2edb5670
Corentin Gerard <corentin_gerard@mbp-de-corentin.lan>
2023-10-30 12:31:06 +0100

    new test

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+/* MAILGUN CONFIGURATION */
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({
   username: "Corentin Gerard",
-  key: process.env.MAILGUN_API_KEY,
+  key: process.env.MAILGUN_API_KEY /* VOTRE CLÉ API */,
+});
+
+app.get("/", async (req, res) => {
+  res.send("server is up");
 });
 
 app.post("/form", async (req, res) => {
   try {
-    const { firstname, lastname, email, subject, message } = req.body;
+    //   Le console.log de req.body nous affiche les données qui ont été rentrées dans les inputs (dans le formulaire frontend) :
+    console.log(req.body);
 
-    if (firstname && lastname && email && message) {
-      const messageData = {
-        from: `${firstname} ${lastname} <${email}>`,
-        to: "gerardpigeaud.corentin@mail.com",
-        subject: subject || `Formulaire JS`,
-        text: message,
-      };
+    // On destructure req.body
+    const { firstname, lastname, email, message } = req.body;
 
-      const response = await client.messages.create(
-        process.env.MAILGUN_DOMAIN,
-        messageData
-      );
+    //   On crée un objet messageData qui contient des informations concernant le mail (qui m'envoie le mail, adresse vers laquelle je veux envoyer le mail, titre et contenu du mail) :
+    const messageData = {
+      from: `${firstname} ${lastname} <${email}>`,
+      to: "gerardpigeaud.corentin@gmail.com",
+      subject: `Formulaire JS`,
+      text: message,
+    };
 
-      res.status(200).json(response);
-    } else {
-      res.status(400).json({ message: "Missing parameters" });
-    }
+    // On envoie les infos à Mailgun pour créer le mail et l'envoyer
+    const response = await client.messages.create(
+      process.env.MAILGUN_DOMAIN, // <==== ⚠️ le domain commence par "sandbox" et fini par ".mailgun.org"
+      messageData
+    );
+
+    console.log(response);
+
+    res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.all("*", (req, res) => {
-  res.json({ message: "All routes" });
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+app.listen(process.env.PORT, () => {
+  console.log("server is listening");
 });
